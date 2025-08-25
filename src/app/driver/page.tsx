91f4e019bfca4a2b67e2da5c07fb0e00489670c8
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-08-25 01:18:22 +0000

    ele me redireciona para a pagina de outro perfil

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -1,9 +1,12 @@
+import { AppLayout } from "@/components/shared/AppLayout";
 import { DriverProfilePage } from "@/components/driver/DriverProfilePage";
 
 export default function Page() {
     return (
-        <div className="bg-muted/40 min-h-screen">
-           <DriverProfilePage />
-        </div>
+        <AppLayout title="Painel do Motorista">
+            <div className="bg-muted/40 min-h-screen">
+               <DriverProfilePage />
+            </div>
+        </AppLayout>
     )
 }
