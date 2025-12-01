f3099c370e7d90440099a20236a5b287f3746be8
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-12-01 20:09:07 +0000

    17:07:04.213 Running build in Washington, D.C., USA (East) – iad1 17:07:

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
-import nextPwa from "@ducanh2912/next-pwa";
 
-const withPWA = nextPwa({
-  dest: "public",
+import withPWAInit from "@ducanh2g/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
       },
     ],
   },
