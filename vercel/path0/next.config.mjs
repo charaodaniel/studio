0be8e0ccc0a9ee9f19ed6ac9b006f795fdb5df7c
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-12-01 20:28:44 +0000

    refatore 17:27:18.941 Running build in Washington, D.C., USA (East) – ia

---
+++ vercel/path0/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+    // any other next.js config options
+};
+
+export default withPWA(nextConfig);
