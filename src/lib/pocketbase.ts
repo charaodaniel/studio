4b099563efdf55deb92c9e956443acbe6245c920
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-12-03 22:12:27 +0000

    https://www.mobmv.shop/_/#/ endereço do painel

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pbUrl = process.env.NEXT_PUBLIC_POCKETBASE_URL || 'https://mobmv.shop';
+const pbUrl = process.env.NEXT_PUBLIC_POCKETBASE_URL || 'https://www.mobmv.shop';
 const pb = new PocketBase(pbUrl);
 
 // globally disable auto cancellation
