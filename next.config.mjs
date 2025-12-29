4d2bd4fa6586353735190ada0a89bf139162d473
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-12-29 17:59:24 +0000

    ○ (pwa)   Scope: /  ✓ Compiled successfully    Linting and checking vali

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // add your own strategies to the existing ones
-  // Your other PWA options goes here
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own strategies to the cache
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
 });
 
 
 const nextConfig = {
-  // Your regular Next.js config options go here
+  // your next config here
 };
 
 export default withPWA(nextConfig);
