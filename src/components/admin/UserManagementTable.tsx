35e4d35a4ed1fd4ee751fbda106efce912431695
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2026-01-10 21:38:41 +0000

    [{ 	"resource": "/home/user/studio/src/components/admin/UserManagementTa

--- src/components/admin/UserManagementTable.tsx
+++ src/components/admin/UserManagementTable.tsx
@@ -16,7 +16,7 @@ import { MoreHorizontal, FileDown, Trash2, Edit, UserPlus, ListVideo, FileText, 
 import { useEffect, useState, useCallback } from "react";
 import type { User } from "./UserList";
 import { useToast } from "@/hooks/use-toast";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "../ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
 import { Dialog, DialogContent, DialogTrigger, DialogHeader, DialogTitle, DialogDescription } from "../ui/dialog";
 import { cn } from "@/lib/utils";
 import AddUserForm from "./AddUserForm";
@@ -440,3 +440,4 @@ export default function UserManagementTable() {
       </>
     );
   }
+
